2d1ed2373c57ad0052291eafaeba6f6db7dafd47
zaforanoi <zaforanoi@gmail.com>
2025-11-12 23:31:56 +0000

    Actuañizar Repositorio

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// Add JS here
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var user = document.getElementById("username");
+    var password = document.getElementById("password");
+    var loginBtn = document.getElementById("loginBtn");
+    var success = document.getElementById("success");
+    var error = document.getElementById("error");
+    if (!user || !password || !loginBtn || !success || !error) {
+        throw Error("Elementos no encontrados");
+    }
+    loginBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (user.value == "usuario" && password.value == "contraseña") {
+            success.style.display = "block";
+            error.style.display = "none";
+        }
+        else {
+            success.style.display = "none";
+            error.style.display = "block";
+        }
+    });
+});
